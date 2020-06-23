15762dded78cd8d2b9c26dacbe6a5ad5a6dbc055
Ruben <401279@Student.fontys.nl>
2020-06-23 15:51:51 +0200

    Initial Code Commit

---
+++ Squid/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Squid.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
